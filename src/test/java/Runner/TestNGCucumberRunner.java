6c89cf625040ccc8d843f9db6e8bfdda035b08bf
alemany89 <alemany89@hotmail.com>
2025-08-03 05:37:40 +0200

    Delete target directory

--- src/test/java/Runner/TestNGCucumberRunner.java
+++ src/test/java/Runner/TestNGCucumberRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/Features",
         glue = {"Steps", "Hooks"},
-        plugin = { "pretty", "html:target/cucumber-reports" }
+        plugin = { "pretty", "html:target/index.html" }
 )
 
 public class TestNGCucumberRunner extends AbstractTestNGCucumberTests  {
