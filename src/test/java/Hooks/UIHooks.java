85c24e14f9066e4df0701455cc554fa59ff590df
alemany89 <alemany89@hotmail.com>
2025-08-03 02:45:25 +0200

    initial commit

--- src/test/java/Hooks/UIHooks.java
+++ src/test/java/Hooks/UIHooks.java
@@ -7,6 +7,10 @@ import io.cucumber.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class UIHooks {
 
     private final World world;
@@ -25,7 +29,16 @@ public class UIHooks {
         ChromeOptions options = new ChromeOptions();
 
         if (headlessMode) {
-            options.addArguments("headless");
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            try {
+                Path tempProfile = Files.createTempDirectory("chrome-profile");
+                options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+            } catch (IOException e) {
+                throw new RuntimeException("No se pudo crear el directorio temporal para Chrome", e);
+            }
         } else {
             options.addArguments("--start-maximized");
         }
